4e84ce8d2fe8e3c34f2256d94b508883c812f54b
ntnon <Anton.nydal@gmail.com>
2025-08-20 09:44:59 +0200

    isolating ui logic

---
+++ src/ui/ui.h
@@ -0,0 +1,47 @@
+#include "third_party/clay.h"
+
+#ifndef UI_STYLES_H
+#define UI_STYLES_H
+extern const Clay_Color M_LIGHTGRAY  ;
+extern const Clay_Color M_GRAY       ;
+extern const Clay_Color M_DARKGRAY   ;
+extern const Clay_Color M_YELLOW     ;
+extern const Clay_Color M_GOLD       ;
+extern const Clay_Color M_ORANGE     ;
+extern const Clay_Color M_PINK       ;
+extern const Clay_Color M_RED        ;
+extern const Clay_Color M_MAROON     ;
+extern const Clay_Color M_GREEN      ;
+extern const Clay_Color M_LIME       ;
+extern const Clay_Color M_DARKGREEN  ;
+extern const Clay_Color M_SKYBLUE    ;
+extern const Clay_Color M_BLUE       ;
+extern const Clay_Color M_DARKBLUE   ;
+extern const Clay_Color M_PURPLE     ;
+extern const Clay_Color M_VIOLET     ;
+extern const Clay_Color M_DARKPURPLE ;
+extern const Clay_Color M_BEIGE      ;
+extern const Clay_Color M_BROWN      ;
+extern const Clay_Color M_DARKBROWN  ;
+extern const Clay_Color M_WHITE      ;
+extern const Clay_Color M_BLACK      ;
+extern const Clay_Color M_BLANK      ;
+extern const Clay_Color M_MAGENTA    ;
+extern const Clay_Color M_RAYWHITE   ;
+// Fonts
+#define FONT_DEFAULT_SPACING 3
+#define MAX_FONTS 8
+extern const uint32_t FONT_ID_LATO;
+
+// Text configs
+extern const Clay_TextElementConfig TEXT_CONFIG_LARGE;
+extern const Clay_TextElementConfig TEXT_CONFIG_MEDIUM;
+
+#endif // UI_STYLES_H
+
+
+#ifndef UI_LAYOUT_H
+#define UI_LAYOUT_H
+
+
+#endif // UI_LAYOUT_H
